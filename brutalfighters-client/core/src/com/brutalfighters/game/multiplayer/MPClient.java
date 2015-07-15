1d9470918cf6c19c0afdf7f0820450d23e08e3e0
Israel Gilyadov <israelg99@gmail.com>
2015-07-15 13:24:20 -0500

    Changed the IP from local host to the game server in San Francisco

--- brutalfighters-client/core/src/com/brutalfighters/game/multiplayer/MPClient.java
+++ brutalfighters-client/core/src/com/brutalfighters/game/multiplayer/MPClient.java
@@ -17,9 +17,9 @@ public class MPClient {
 		client = new Client(33668, 4096);
 		client.start();
 		try {
-			//client.connect(5000, "188.226.195.78", 54777, 54666); // OLD_OUTDATED_DIGITALOCEAN_EU
-			//client.connect(5000, "107.170.243.67", 54777, 54666); // NA_SanFrancisco_DigitalOcean
-			client.connect(5000, "127.0.0.1", 54777, 54666); //$NON-NLS-1$
+			//client.connect(5000, "188.226.195.78", 54777, 54666); // OLD_OUTDATED_DIGITALOCEAN_EU //$NON-NLS-1$
+			client.connect(5000, "107.170.243.67", 54777, 54666); // NA_SanFrancisco_DigitalOcean //$NON-NLS-1$
+			//client.connect(5000, "127.0.0.1", 54777, 54666); //$NON-NLS-1$
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(null, "Cannot connect to the server, Maybe the server isn't started yet"); //$NON-NLS-1$
 		}
