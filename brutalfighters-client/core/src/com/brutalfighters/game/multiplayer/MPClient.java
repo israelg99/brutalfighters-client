7cdb5bf4be5d272ac364e9d21a34c8b15a99874c
Israel Gilyadov <israelg99@gmail.com>
2015-10-18 00:13:50 -0500

    San Francisco VPS IP update..

--- brutalfighters-client/core/src/com/brutalfighters/game/multiplayer/MPClient.java
+++ brutalfighters-client/core/src/com/brutalfighters/game/multiplayer/MPClient.java
@@ -18,8 +18,8 @@ public class MPClient {
 		client.start();
 		try {
 			//client.connect(5000, "188.226.195.78", 54777, 54666); // OLD_OUTDATED_DIGITALOCEAN_EU //$NON-NLS-1$
-			//client.connect(5000, "107.170.243.67", 54777, 54666); // NA_SanFrancisco_DigitalOcean //$NON-NLS-1$
-			client.connect(5000, "127.0.0.1", 54777, 54666); //$NON-NLS-1$
+			client.connect(5000, "107.170.243.67", 54777, 54666); // NA_SanFrancisco_DigitalOcean //$NON-NLS-1$
+			//client.connect(5000, "127.0.0.1", 54777, 54666); //$NON-NLS-1$
 		} catch (IOException e) {
 			JOptionPane.showMessageDialog(null, "Cannot connect to the server, Maybe the server isn't started yet"); //$NON-NLS-1$
 		}
